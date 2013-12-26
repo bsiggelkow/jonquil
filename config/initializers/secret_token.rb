495a76b5ab25d7ad90763deda74e225bcd205ab6
Bill Siggelkow <bill.siggelkow@inbloom.org>
2013-12-26 12:37:08 -0500

    Initial rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jonquil::Application.config.secret_token = '8c55658026d33085556e4b6a37a804e7d3ccaed0ac0e9c02a96672f2622060f48dacdc89ffc989c13fd610c0022b1416fe3e352cbc435a30bf9322ca3454ff05'
